aeab0e944cb4d6734c9d6355957ed8d5400ac951
Hudson H <Hudsonhargrove01@gmail.com>
2026-03-17 20:58:48 -0500

    update red tests to unittest framework

--- 2/test.py
+++ 2/test.py
@@ -1,19 +1,26 @@
-def test_BMI_value():
-    BMI = BMI("5 10", 150)
-    assert BMI == 21.5
+import unittest
 
-    BMI = BMI("6 0", 200)
-    assert BMI == 27.1
+class test_BMI_value(unittest.TestCase):
+    def test_BMI_value(self):
+        BMI = BMI("5 10", 150)
+        self.assertEqual(BMI, 21.5)
 
-    BMI = BMI("5 5", 120)
-    assert BMI == 20.0
+        BMI = BMI("6 0", 200)
+        self.assertEqual(BMI, 27.1)
 
-def test_BMI_category():
-    assert BMI_category(0.0) == "Underweight"
-    assert BMI_category(18.4) == "Underweight"
-    assert BMI_category(18.5) == "Normal weight"
-    assert BMI_category(24.9) == "Normal weight"
-    assert BMI_category(25.0) == "Overweight"
-    assert BMI_category(29.9) == "Overweight"
-    assert BMI_category(30.0) == "Obese"
-    assert BMI_category(100.0) == "Obese"
+        BMI = BMI("5 5", 120)
+        self.assertEqual(BMI, 20.0)
+
+class test_BMI_category(unittest.TestCase):
+    def test_BMI_category(self):
+        self.assertEqual(BMI_category(0.0), "Underweight")
+        self.assertEqual(BMI_category(18.4), "Underweight")
+        self.assertEqual(BMI_category(18.5), "Normal weight")
+        self.assertEqual(BMI_category(24.9), "Normal weight")
+        self.assertEqual(BMI_category(25.0), "Overweight")
+        self.assertEqual(BMI_category(29.9), "Overweight")
+        self.assertEqual(BMI_category(30.0), "Obese")
+        self.assertEqual(BMI_category(100.0), "Obese")
+
+if __name__ == '__main__':
+    unittest.main()
